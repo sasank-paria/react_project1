b7abfa1036ba222ab4a6f41099afa7311e610835
paria <83511327+sasank-paria@users.noreply.github.com>
2023-01-03 15:54:18 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { BrowserRouter as Route,Router,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
 
-
+import "./styles/app.scss";
+import "./styles/color.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/mediaquery.scss";
 
 
 function App() {
   return (
     <Router>
+    <Header/>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/services" element={<Services/>}/>
       </Routes>
+      <Footer/>
     </Router>
-  );
+  )
 }
 
 export default App;
